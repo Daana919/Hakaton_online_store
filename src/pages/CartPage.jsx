1a0adb35971e9eea8720e4c2fe3f1877e8db839e
Rebllrays <aigeraphr13@gmail.com>
2023-03-01 14:24:24 +0600

    context_pages

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Cart from "../components/Cart/Cart";
 
 const CartPage = () => {
-	return <div>CartPage</div>;
+  return (
+    <div>
+      <h3>Cart Page</h3>
+      <Cart />
+    </div>
+  );
 };
 
 export default CartPage;
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ProductDetails from "../components/products/ProductDetails/ProductDetails";
 
 const ProductDetailsPage = () => {
-	return <div>ProductDetailsPage</div>;
+  return (
+    <>
+      <ProductDetails />
+    </>
+  );
 };
 
 export default ProductDetailsPage;
