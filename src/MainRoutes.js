e28d742ee140d9de86f058068f9acbd8e0375be1
Rebllrays <aigeraphr13@gmail.com>
2023-03-01 14:13:01 +0600

    nav

--- src/MainRoutes.js
+++ src/MainRoutes.js
@@ -5,72 +5,67 @@ import LoginPage from "./pages/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import HomePage from "./pages/HomePage";
 import AdminPage from "./pages/AdminPage";
-import ProductDetailsPage from "./pages/ProductDetailsPage";
-import EditPage from "./pages/EditPage";
 import ProductsPage from "./pages/ProductsPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
+import EditProductPage from "./pages/EditProductPage";
 import CartPage from "./pages/CartPage";
-import FormOrderPage from "./pages/FormOrderPage";
 
 const MainRoutes = () => {
-	const PUBLIC_ROUTES = [
-		{
-			link: "/register",
-			element: <RegistrationPage />,
-			id: 1,
-		},
-		{
-			link: "/login",
-			element: <LoginPage />,
-			id: 2,
-		},
-		{
-			link: "/",
-			element: <HomePage />,
-			id: 3,
-		},
-		{
-			link: "*",
-			element: <NotFoundPage />,
-			id: 4,
-		},
-		{
-			link: "/admin",
-			element: <AdminPage />,
-			id: 5,
-		},
-		{
-			link: "/products",
-			element: <ProductsPage />,
-			id: 6,
-		},
-		{
-			link: "/details/:id",
-			element: <ProductDetailsPage />,
-			id: 7,
-		},
-		{
-			link: "/edit/:id",
-			element: <EditPage />,
-			id: 8,
-		},
-		{
-			link: "/cart",
-			element: <CartPage />,
-			id: 9,
-		},
-		{
-			link: "/order",
-			element: <FormOrderPage />,
-			id: 10,
-		},
-	];
-	return (
-		<Routes>
-			{PUBLIC_ROUTES.map(item => (
-				<Route path={item.link} element={item.element} key={item.id} />
-			))}
-		</Routes>
-	);
+  const PUBLIC_ROUTES = [
+    {
+      link: "/register",
+      element: <RegistrationPage />,
+      id: 1,
+    },
+    {
+      link: "/login",
+      element: <LoginPage />,
+      id: 2,
+    },
+    {
+      link: "/",
+      element: <HomePage />,
+      id: 3,
+    },
+    {
+      link: "*",
+      element: <NotFoundPage />,
+      id: 4,
+    },
+    {
+      link: "/admin",
+      element: <AdminPage />,
+      id: 5,
+    },
+    {
+      link: "/products",
+      element: <ProductsPage />,
+      id: 6,
+    },
+    {
+      link: "/details/:id",
+      element: <ProductDetailsPage />,
+      id: 7,
+    },
+    {
+      link: "/edit/:id",
+      element: <EditProductPage />,
+      id: 8,
+    },
+    {
+      link: "/cart",
+      element: <CartPage />,
+      id: 9,
+    },
+  ];
+
+  return (
+    <Routes>
+      {PUBLIC_ROUTES.map((item) => (
+        <Route path={item.link} element={item.element} key={item.id} />
+      ))}
+    </Routes>
+  );
 };
 
 export default MainRoutes;
